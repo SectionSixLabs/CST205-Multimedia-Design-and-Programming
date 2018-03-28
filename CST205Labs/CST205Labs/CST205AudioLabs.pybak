1497a017feba1aa353985949f3e73d3b4267db27
Sergiy Zarubin <live@roguesurge.com>
2018-03-27 18:42:37 -0700

    blah

--- CST205Labs/CST205Labs/CST205AudioLabs.pybak
+++ CST205Labs/CST205Labs/CST205AudioLabs.pybak
@@ -1,6 +1,7 @@
 #Sergiy Zarubin
 #Danny Tran
-
+#Specific to each student based opn the environment 
+DIR =r"D:\\Users\\live\Source\\Repos\\CST205-Multimedia-Design-and-Programming\\CST205Labs\\CST205Labs\\"
 #LAB8      
 #Warm Up      
       
@@ -69,27 +70,127 @@ def goToEleven(sound):
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 def clip(source,start,end):
   sourceSampleRate =int( getSamplingRate(source))
-  print sourceSampleRate
-  numberSamples = end-start
-  print numberSamples
+  printNow (sourceSampleRate)
+  printNow (len(getSamples(source)))
+  numberSamples = end-start+1
+  printNow (numberSamples)
   newClip = makeEmptySound (numberSamples,sourceSampleRate)
-  for sampleIndex in range(start,end):
+  for sampleIndex in range(start,end-2):
     sourceVolume = getSampleValueAt(source, sampleIndex) 
     setSampleValueAt(newClip,sampleIndex-start,sourceVolume)
   return maxVolume(newClip)
 
+def SampleDown(source,coeficient):
+
+    sourceSamples =getSamples(source)
+    newSampleRate = int(getSamplingRate(source))//coeficient
+    numSourceSamples = len(sourceSamples)
+    
+    index=0
+    newClip = makeEmptySound (numSourceSamples//coeficient,newSampleRate)
+    
+    for sampleIndex in range(0,numSourceSamples,coeficient):
+      sourceVolume = getSampleValueAt(source, sampleIndex) 
+      setSampleValueAt(newClip,index,sourceVolume)
+      index=index+1
+    return newClip
+
 def copy(source, target, start):
     sourceSamples =getSamples(source)
     targetSamples = getSamples(target)
-    numSourceSamples = len(sourceSamples)
-    numTargetSamples = len(targetSamples)
     
-def reverse(sound):
-  return ""
+    sourceRate = int(getSamplingRate(source))
+    targetRate = int(getSamplingRate(source))
+    if sourceRate==targetRate:
+
+      newSampleRate = targetRate
+      coeficient = 1
+      numSourceSamples = len(sourceSamples)
+      numTargetSamples = len(targetSamples)
+      
+    elif sourceRate>targetRate:
+    
+      newSampleRate = targetRate
+      coeficient =  targetRate//sourceRate
+      source = SampleDown(source,coeficient)
+      numSourceSamples = len(sourceSamples)
+      numTargetSamples = len(targetSamples)
+      sourceSamples =getSamples(source)
+      
+    elif sourceRate<targetRate:
+    
+      newSampleRate = sourceRate
+      coeficient =  sourceRate//targetRate
+      source = SampleDown(source,coeficient)
+      numSourceSamples = len(sourceSamples)
+      numTargetSamples = len(targetSamples)
+      targetSamples = getSamples(target)
+      
+    newClip = makeEmptySound (numSourceSamples+numTargetSamples,newSampleRate)
+    numNewClipSamples = len(getSamples(newClip))
+    
+    sourceIndex = 0
+    targetIndex = start+1
+    
+    for sampleIndex in range (0,numNewClipSamples-1):
+      if sampleIndex<start:
+        targetVolume = getSampleValueAt(target, sampleIndex) 
+        setSampleValueAt(newClip,sampleIndex,targetVolume)
+      elif sourceIndex<numSourceSamples:
+        sourceVolume = getSampleValueAt(source, sourceIndex) 
+        setSampleValueAt(newClip,sampleIndex,sourceVolume)
+        sourceIndex=sourceIndex+1
+      elif sourceIndex>=numSourceSamples  and sampleIndex>numNewClipSamples:
+        targetVolume = getSampleValueAt(target, targetIndex) 
+        setSampleValueAt(newClip,sampleIndex,targetVolume)
+        targetIndex=targetIndex+1
+    return newClip  
+def makeaSound(): 
+  filePath1 =r"LAB9\\tdnpss02.wav"
+  filePath2 =r"LAB9\\tmapss02.wav"
+  filePath3 =r"LAB9\\tmapss05.wav"
+  filePath4 =r"LAB9\\tphpss01.wav"
+  filePath5 =r"LAB9\\tvkwht02.wav"
+  #Set input and output path
+  fileIn1 =DIR+r"Original\\"+filePath1
+  fileIn2 =DIR+r"Original\\"+filePath2
+  fileIn3 =DIR+r"Original\\"+filePath3
+  fileIn4 =DIR+r"Original\\"+filePath4
+  fileIn5 =DIR+r"Original\\"+filePath5
+  #print fileIn
+  #Initialise the sound 
+  sound1 = makeSound(fileIn1)
+  #explore(sound1)
+  sound2 = makeSound(fileIn2)
+  #explore(sound2)
+  sound3 = makeSound(fileIn3)
+  #explore(sound3)
+  sound4 = makeSound(fileIn4)
+  #explore(sound4)
+  sound5 = makeSound(fileIn5)
+  #explore(sound5)
+  newClip= copy(sound2,sound3,14219)
+  newClip= copy(sound4,newClip,48927)
+  newClip= copy(clip(sound1,31408,53976),newClip,81968)
+  newClip= copy(clip(sound5,4964,40442),newClip,105501)
+  newClip =clip(newClip,0,145501)
+  return newClip
+
+def reverse(source):
+   sourceSamples =getSamples(source)
+   numSourceSamples = len(sourceSamples)
+   newSampleRate = int(getSamplingRate(source))
+   newClip = makeEmptySound (numSourceSamples,newSampleRate)
+   index=0
+   endIndex = numSourceSamples-1
+   for sample in sourceSamples:
+     sourceVolume = getSampleValueAt(source, index) 
+     setSampleValueAt(newClip,endIndex-index,sourceVolume)
+     index=index+1
+   return maxVolume(newClip)
   
 def Run():
-  #Specific to each student based opn the environment 
-  dir =r"D:\\Users\\live\Source\\Repos\\CST205-Multimedia-Design-and-Programming\\CST205Labs\\CST205Labs\\"
+
 #LAB8
   #Now you try it:
   #filePath =r"LAB8\\shirley.wav"
@@ -105,17 +206,12 @@ def Run():
   #filePath =r"LAB8\\maxVolume.wav"
   #goToEleven
   #filePath =r"LAB8\\goToEleven.wav"
-#LAB9
-  filePath =r"LAB9\\cantdo.wav"
   #Set input and output path
-  fileIn =dir+r"Original\\"+filePath
-  fileOut =dir+r"Result\\"+filePath
-  print fileIn
+  #fileIn =DIR+r"Original\\"+filePath
+  #fileOut =DIR+r"Result\\"+filePath
+  #print fileIn
   #Initialise the sound 
-  sound = makeSound(fileIn)
-  explore(sound)
-  newSound = clip(sound,0,16154)
-  explore(newSound)
+  #sound = makeSound(fileIn)
   #Now you try it:
   #print retrieveSample(sound, 10000)
   #increaseVolume(sound)
@@ -126,6 +222,23 @@ def Run():
   #maxVolume(sound)
   #goToEleven(sound)
   #writeSoundTo(sound, fileOut)
-  #explore(sound)
+#LAB9
+  filePath =r"LAB9\\cards.wav"
+  fileIn =DIR+r"Original\\"+filePath
+  sound = makeSound(fileIn)
+  #newClip=reverse(sound)
+  #explore(newClip)
+  newClip=reverse(sound)
+  #explore(newClip)
+  #Sound 1 (0 - 11232) But I think
+  #32760-53976 Too Familiar
+  #Sound 2 0-24272 Oh My God
+  #25912 - 49692 he's wet
+  #Sound 3 0-14219 You wana pee 48927 81968
+  #explore(newSound)
+  #fileOut =DIR+r"Result\\LAB9\\ReversedCards.wav"
+  fileOut =DIR+r"Result\\LAB9\\NewSounds.wav"
+  #writeSoundTo(newClip, fileOut)
+  writeSoundTo(makeaSound(), fileOut)
   
     
--- CST205Labs/MidTermIGFilters/MidTermIGFilters.py
+++ CST205Labs/MidTermIGFilters/MidTermIGFilters.py
@@ -3,122 +3,52 @@
 #Not sure what to do with the original image yet, Possibly make it monochrome
 #Or possibly artified black / white...
 
-def pyCopy(source, target, targetX, targetY): #source need be a makePicture object parameter
-  #pic = makePicture(source)  #input course should be file directory path
-  #above is commented out so that source is now a makepicture parameter
-  #that way use functions that pipe into pyCopy
-
-  pic = source
-  pwidth = getWidth(pic)
-  pheight = getHeight(pic)
-
-  for x in range(0, pwidth):
-    for y in range(0, pheight):
-      colorSource = getColor(getPixel(pic, x, y)) #get color pixel of source
-      targetPixel = getPixel(target, x+targetX, y+targetY) #target pixel
-      setColor(targetPixel, colorSource)
-
-def logoCopy(target, targetX, targetY): #source need be a makePicture object parameter
-  #pic = makePicture(source)  #input course should be file directory path
-  #above is commented out so that source is now a makepicture parameter
-  #that way use functions that pipe into pyCopy
-
-  logo = makePicture('/home/wave/Downloads/gitWorkspace/FilterIdeas/logo.png')
-  pwidth = getWidth(logo)
-  pheight = getHeight(logo)
-
-  wPixel = makeColor(255, 255, 255) #Pixel to ignore in logo (this is white)
-
-
-  for x in range(0, pwidth):
-    for y in range(0, pheight):
-      colorSource = getColor(getPixel(logo, x, y)) #get color pixel of source
-      targetPixel = getPixel(target, x+targetX, y+targetY) #target pixel
-      if(colorSource != wPixel):
-        setColor(targetPixel, colorSource)
-
-def advance(source):
-  
-  pic = source
-  #make pic into black and white image
-  pixels = getPixels(pic)
-  for p in pixels:
-    redP = getRed(p)
-    blueP = getBlue(p)
-    greenP = getGreen(p)
-    averageP = redP*0.299 + greenP*0.587 + blueP*0.114
-    makeGrey = makeColor(averageP, averageP, averageP)
-    setColor(p, makeGrey)
-
-
-  width = getWidth(pic)
-  height = getHeight(pic)
-   
-  for x in range(0, width-1):
-    for y in range(0, height-1):
-      currentPixel = getPixel(pic, x, y)
-      belowPixel = getPixel(pic, x, y+1)
-      rightPixel = getPixel(pic, x+1, y)
-      
-      #luminescence 
-      currentPixelLum = (getRed(currentPixel) + getGreen(currentPixel) + getBlue(currentPixel))/3
-      belowPixelLum = (getRed(belowPixel) + getGreen(belowPixel) + getBlue(belowPixel))/3
-      rightPixelLum = (getRed(rightPixel) + getGreen(rightPixel) + getBlue(rightPixel))/3
-  
-      #from trying various differenceValues, increasing number resulted in less filled in black pixels and vice versa
-      differenceValue = 5
-      if( abs(currentPixelLum - belowPixelLum) > differenceValue and abs(currentPixelLum - rightPixelLum) > differenceValue):
-        setColor(currentPixel, black)
-
-      if( abs(currentPixelLum - belowPixelLum) <= differenceValue and abs(currentPixelLum - rightPixelLum) <= differenceValue):
-        setColor(currentPixel, white) 
-  
-  return pic
-
-
-def filter():
-    file = pickAFile()
-    pic = makePicture(file)
-    width = getWidth(pic)
-    height = getHeight(pic)
-    if(height < 250): #250 is logo height
-      print("Image height too small")
-      return None
-    if(width < 685): #685 is logo width
-      print("Image width is too small")
-      return None
-
-    canvas = makeEmptyPicture(width, height)
-    pyCopy(advance(pic), canvas, 0, 0)
-
-    logoCopy(canvas, 0, height-250) #logo height 250
-
-    show(canvas)
-    writePictureTo(canvas, '/home/wave/Downloads/gitWorkspace/FilterIdeas/filterOutput.jpg')
-    return(canvas)
-
+def logoCopy (source, backGround, coordX, coordY,color2ignor): 
+  sourceW,sourceH = getDimantions(source)
+  bgW,bgH = getDimantions(backGround)
+  if (sourceW+coordX)>bgW or (sourceH+coordY)>bgH :
+    return errorMSG(source,"YELL NO "+str(sourceW)+"x"+str(sourceH)+" "+str(coordX)+":"+str(coordY)+" "+str(bgW)+"x"+str(bgH))
+  for x in range (0, bgW):
+   for y in range (0, bgH):
+    if x>=coordX and x<sourceW+coordX and y>=coordY and y<sourceH+coordY:
+     picPixel=getPixelAt(backGround,x,y)
+     pixelX=x-coordX
+     pixelY=y-coordY
+     pixel = getPixelAt(source,pixelX,pixelY)
+     pixelRed = getRed(pixel) 
+     pixelGreen = getGreen(pixel)
+     pixelBlue = getBlue(pixel)
+     newPicColor = makeColor( pixelRed,pixelGreen,pixelBlue)
+     if newPicColor!=color2ignor:
+      setColor(picPixel,newPicColor)
+  return backGround
 
 def getDimantions(pic):
      widthPic= getWidth(pic)
      heightPic = getHeight(pic)
      return widthPic, heightPic
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 def shrincPic(pic):
   widthPic, heightPic = getDimantions(pic)
-  newPic = makeEmptyPicture(widthPic//2,heightPic//2)
+  newPic = makeEmptyPicture((widthPic+1)//2,(heightPic+1)//2)
   for x in range (0, widthPic,2):
     for y in range (0, heightPic,2):
       pixel = getPixelAt(pic,x,y)
-      newPixel  = getPixelAt(newPic,x/2,y/2)
+      newPixel  = getPixelAt(newPic,x//2,y//2)
       color2Copy= getColor(pixel)
       setColor(newPixel,color2Copy)
-  return newPic 
+  return newPic
 
 def blowUpPic(pic):
   widthPic, heightPic = getDimantions(pic)
-  newPic = makeEmptyPicture(widthPic*2,heightPic*2)
-  for x in range (0, widthPic*2):
-   for y in range (0, heightPic*2):
+  newW = widthPic*2
+  newH = heightPic*2
+  printNow(str(newW)+"X"+str(newH))
+  newPic = makeEmptyPicture(newW,newH)
+  for x in range (0, newW):
+   for y in range (0, newH):
       pixel = getPixelAt(pic,x//2,y//2)
       newPixel  = getPixelAt(newPic,x,y)
       color2Copy= getColor(pixel)
@@ -153,3 +83,26 @@ def filter8bit(pic):
   pic = blowUpPic(pic)
   return (pic)
 
+def makeGameSplash(pic,gameName):
+    dir =r"D:\\Users\\live\Source\\Repos\\CST205-Multimedia-Design-and-Programming\\CST205Labs\\\MidTermIGFilters\\"
+    filePath =r"activision.jpg"
+    fileIn =dir+r"Originals\\"+filePath
+    logoBaseColor = makeColor(25,25,25)
+    logo = makePicture(fileIn)
+    logoW,logoH = getDimantions(logo)
+    picW,picH = getDimantions(pic)
+    pic= logoCopy(logo,pic,(picW-logoW-1)//2, picH-logoH-1,logoBaseColor)
+    import java.awt.Font as Font
+    myFont = makeStyle("Times New Roman", Font.BOLD, 84)
+    addTextWithStyle(pic,200,200,gameName,myFont,red)
+    pic = filter8bit(pic)
+    return pic
+
+def Run():
+    dir =r"D:\\Users\\live\Source\\Repos\\CST205-Multimedia-Design-and-Programming\\CST205Labs\\\MidTermIGFilters\\"
+    filePath =r"IMG_0097.jpg"
+    fileIn =dir+r"Originals\\"+filePath
+    fileOut =dir+r"Result\\"+filePath
+    pic = makePicture(fileIn)
+    pic = makeGameSplash(pic,"Wedding Crashers")
+    writePictureTo(pic,fileOut)
