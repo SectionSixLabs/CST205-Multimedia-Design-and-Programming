55fbd85559173877cf9666cd27c7c5081d2d2437
Sergiy Zarubin <live@roguesurge.com>
2018-03-31 11:10:42 -0700

    Game Code

--- CST205Labs/PythonApplication1/Game.py
+++ CST205Labs/PythonApplication1/Game.py
@@ -9,14 +9,29 @@ def getPlayerCommand():
     return input('Action: ')
 
 def processAction(action):
-    
+    #Processing Palyer Action
+    if action in ['n', 'N']:
+         print("Going North!")
+    elif action in ['s', 'S']:
+         print("Going South!")
+    elif action in ['e', 'E']:
+         print("Going East!")
+    elif action in ['w', 'W']:
+         print("Going West!")
+    elif action in ['i', 'I']:
+         print("Inventory:")
+         for item in inventory:
+             print('* ' + str(item))
+    else:
+         print("Invalid action!")
 
 def Game():
-    inventory= []
+    inventory= ['Spear','Knife','Healing Herbs']
     print (WELCOME)
     isExit=False
     while not isExit:
         actionInput= getPlayerCommand()
+        processAction(actionInput)
         isExit=True
 
 
